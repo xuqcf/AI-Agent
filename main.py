6daf6ed4b624e2856344e7ce50b030adee9ed788
xuqcf <sevt33nclothingbrand@gmail.com>
2026-02-09 17:10:12 -0500

    Added command line arguments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,7 +14,7 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
-prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+prompt = args.user_prompt
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents=prompt
 )
