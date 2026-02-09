ae762b681ddb95fb26c143558efd8646634ec619
xuqcf <sevt33nclothingbrand@gmail.com>
2026-02-09 17:39:45 -0500

    added another command line argument, verbose to see context and token usage

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
+import argparse
+
+from google.genai import types
 from dotenv import load_dotenv
 from google import genai
-import argparse
+
+
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 load_dotenv()
@@ -14,22 +19,25 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
-prompt = args.user_prompt
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=prompt
+    model='gemini-2.5-flash', contents=messages
 )
 
 if response.usage_metadata is None:
     raise RuntimeError("Response usage_metadata is None. The API request may have failed.")
 
-print(f"User prompt: {prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-print("Response:")
-print(response.text)
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+else:
+    print(response.text)
 
 def main():
-    print("Hello from ai-agent!")
+    pass
 
 
 if __name__ == "__main__":
