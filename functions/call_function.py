d401118cf17251930b5d1f49534c23586cf3bce7
xuqcf <sevt33nclothingbrand@gmail.com>
2026-04-06 17:53:07 -0400

    CH3 : L3: More Declarations completed

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,6 +1,15 @@
 from google.genai import types
+
 from functions.get_files_info import schema_get_files_info
+from functions.get_files_contents import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.writing_files import schema_write_file
 
 available_functions = types.Tool(
-    function_declarations=[schema_get_files_info],
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ],
 )
--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -48,7 +48,9 @@ schema_run_python_file = types.FunctionDeclaration(
             ),
             "args": types.Schema(
                 type=types.Type.ARRAY,
-                description="An optional array of strings to be used as the CLI args for the Python file",
+                items=types.Schema(
+                    type=types.Type.STRING,
+                )
             ),
         },
     ),
